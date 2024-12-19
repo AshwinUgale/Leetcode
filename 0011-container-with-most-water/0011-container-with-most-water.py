33b718da2ef5885211009003c9dafacd45cd23cc
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2024-12-19 09:55:12 -0500

    Time: 117 ms (18.09%), Space: 20.8 MB (6.54%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        res=0
+        l=0
+        r=len(height)-1
+        while l<r:
+            area=(r-l)*min(height[l],height[r])
+            res=max(area,res)
+            if height[l]>height[r]:
+                r-=1
+            else:
+                l+=1
+        return res
+        
