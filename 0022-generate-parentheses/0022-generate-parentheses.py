07375e8c480ae1c750a2f71cc31073581f437e4e
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-01-04 08:23:05 -0500

    Time: 1 ms (62.25%), Space: 12.7 MB (22.14%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+       
+        stack=[]
+        res=[]
+        def backtrack(openN,closeN):
+            if openN==closeN==n:
+                res.append("".join(stack))
+                return res
+            if openN<n:
+                stack.append("(")
+                backtrack(openN+1,closeN)
+                stack.pop()
+            if closeN<openN:
+                stack.append(")")
+                backtrack(openN,closeN+1)
+                stack.pop()
+        backtrack(0,0)
+        return res
