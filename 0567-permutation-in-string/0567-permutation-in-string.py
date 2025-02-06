63f3881789a9eb7588f0116a97a053885edf759f
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-02-06 15:56:59 -0500

    Time: 13 ms (89.07%), Space: 17.9 MB (54.33%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -4,21 +4,25 @@ class Solution:
             return False
 
         arr1=[0]*26
+        arr2=[0]*26
         
         for i in range(len(s1)):
             arr1[ord(s1[i])-ord('a')]+=1
+            arr2[ord(s2[i])-ord('a')]+=1
+
+        if arr1==arr2:
+            return True
         
         l=0
-        r=len(s1)-1
-        while r<len(s2):
-            arr2=[0]*26
-            for i in range(l,r+1):
-                arr2[ord(s2[i])-ord('a')]+=1
-      
+        for r in range(len(s1),len(s2)):
+            arr2[ord(s2[r])-ord('a')]+=1
+            arr2[ord(s2[l])-ord('a')]-=1
+            l+=1
+        
             if arr1==arr2:
                 return True
-            l+=1
-            r+=1
+              
+      
         return False
             
         
