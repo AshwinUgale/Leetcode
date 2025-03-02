74a31eabce2348b19e4d9ef3360044b3bc3a3598
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-03-02 12:10:59 -0500

    Time: 0 ms (100%), Space: 18.3 MB (32.2%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.py
+++ 1184-car-pooling/1184-car-pooling.py
@@ -2,17 +2,17 @@ class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         # O(n) solution, for the case when we know the trip length as given in this question
 
-        # passChange = [0] * 1001
-        # for t in trips:
-        #     numPass,start,end = t
-        #     passChange[start] += numPass
-        #     passChange[end] -= numPass
-        # curPass=0
-        # for i in range(1001):
-        #     curPass += passChange[i]
-        #     if curPass > capacity :
-        #         return False
-        # return True
+        passChange = [0] * 1001
+        for t in trips:
+            numPass,start,end = t
+            passChange[start] += numPass
+            passChange[end] -= numPass
+        curPass=0
+        for i in range(1001):
+            curPass += passChange[i]
+            if curPass > capacity :
+                return False
+        return True
 
 
         
@@ -20,17 +20,17 @@ class Solution:
         # O(nlogn) solution
 
 
-        trips.sort(key = lambda t: t[1])
+        # trips.sort(key = lambda t: t[1])
 
-        minHeap = []
-        curPass = 0
-        for t in trips:
-            numPass,start,end=t
-            while minHeap and minHeap[0][0] <= start:
-                curPass-=minHeap[0][1]
-                heapq.heappop(minHeap)
-            curPass += numPass
-            if curPass > capacity:
-                return False
-            heapq.heappush(minHeap,[end,numPass])
-        return True
+        # minHeap = []
+        # curPass = 0
+        # for t in trips:
+        #     numPass,start,end=t
+        #     while minHeap and minHeap[0][0] <= start:
+        #         curPass-=minHeap[0][1]
+        #         heapq.heappop(minHeap)
+        #     curPass += numPass
+        #     if curPass > capacity:
+        #         return False
+        #     heapq.heappush(minHeap,[end,numPass])
+        # return True
