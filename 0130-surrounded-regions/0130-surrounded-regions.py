ac7da12b479a26ecc56726d914ee62185152a479
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-03-02 18:18:03 -0500

    Time: 13 ms (23.45%), Space: 21.9 MB (42.73%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -0,0 +1,29 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS = len(board)
+        COLS = len(board[0])
+
+        unSur = set()
+
+        def countS(r,c):
+            if (r < 0 or c < 0 or r == ROWS or c == COLS
+                or board[r][c]!="O" or (r, c) in unSur):
+                return 
+            unSur.add((r,c))
+            countS(r+1,c)
+            countS(r-1,c)
+            countS(r,c+1)
+            countS(r,c-1)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (board[r][c] == "O" and 
+                    (r in [0,ROWS-1] or c in  [0,COLS-1])):
+                    countS(r,c)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O" and (r,c) not in unSur:
+                    board[r][c] = "X"
+
+            
