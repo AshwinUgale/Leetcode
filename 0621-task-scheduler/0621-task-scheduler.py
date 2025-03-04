22e996834c4dc84c7e51b5d46e68cd10cc066a5e
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-03-03 20:30:19 -0500

    Time: 147 ms (36.9%), Space: 19.3 MB (8.2%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -3,14 +3,16 @@ class Solution:
         count = Counter(tasks)
         maxHeap = [-cnt for cnt in count.values()]
         heapq.heapify(maxHeap)
-        time = 0
         q = deque()
+        time = 0
+
         while maxHeap or q:
-            time +=1
+            time += 1
             if maxHeap:
-                cnt =1+heapq.heappop(maxHeap)
-                if cnt:
-                    q.append([cnt, time +n])
+                last = heapq.heappop(maxHeap)
+                if last+1:
+                    q.append([last+1, time + n])
             if q and q[0][1] == time:
                 heapq.heappush(maxHeap,q.popleft()[0])
         return time
+
