7c1c1c5ae1a97f3672b41d76f8e820fb92b6e25d
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-03-04 21:09:06 -0500

    Time: 3 ms (66.64%), Space: 18.4 MB (31.82%) - LeetHub

--- 1184-car-pooling/1184-car-pooling.py
+++ 1184-car-pooling/1184-car-pooling.py
@@ -1,36 +1,32 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        # O(n) solution, for the case when we know the trip length as given in this question
-
-        passChange = [0] * 1001
+        #O(n)
+        changePass= [0] * 1001
         for t in trips:
-            numPass,start,end = t
-            passChange[start] += numPass
-            passChange[end] -= numPass
+            numpass,s,e=t
+            changePass[s]+=numpass
+            changePass[e]-=numpass
         curPass=0
         for i in range(1001):
-            curPass += passChange[i]
-            if curPass > capacity :
+            curPass+=changePass[i]
+            if curPass>capacity:
                 return False
         return True
 
-
         
         
-        # O(nlogn) solution
-
-
-        # trips.sort(key = lambda t: t[1])
-
+        #O(nlogn)
+        # trips.sort(key = lambda t:t[1])
         # minHeap = []
         # curPass = 0
         # for t in trips:
-        #     numPass,start,end=t
+        #     numPass,start,end = t
         #     while minHeap and minHeap[0][0] <= start:
         #         curPass-=minHeap[0][1]
         #         heapq.heappop(minHeap)
-        #     curPass += numPass
+        #     curPass+=numPass
         #     if curPass > capacity:
         #         return False
-        #     heapq.heappush(minHeap,[end,numPass])
+        #     heapq.heappush(minHeap,[end,curPass])
         # return True
+       
