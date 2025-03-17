aa2ae96695561c2bb347e9cf119f49f192150bc5
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-03-17 15:27:26 -0400

    Time: 3 ms (6.78%), Space: 19 MB (5.54%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -17,9 +17,9 @@ class Solution:
         #Recursive
         if not head:
             return None
-        newHead = head
-        if head.next:
-            newHead = self.reverseList(head.next)
-            head.next.next = head
+        if not head.next:
+            return head
+        newHead = self.reverseList(head.next)
+        head.next.next = head
         head.next = None
         return newHead
