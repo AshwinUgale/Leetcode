cf010a91b0e1833c88981af3d1e9ea63b3edc005
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-03-21 13:52:31 -0400

    Time: 15 ms (85.32%), Space: 17.8 MB (62.84%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,30 +1,36 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1) > len(s2):
+        if len(s1)>len(s2):
             return False
 
-        arr1=[0]*26
-        arr2=[0]*26
-        
+        s1Count,s2Count = [0]*26 , [0]*26
         for i in range(len(s1)):
-            arr1[ord(s1[i])-ord('a')]+=1
-            arr2[ord(s2[i])-ord('a')]+=1
-
-        if arr1==arr2:
-            return True
+            s1Count[ord(s1[i]) - ord('a')]+=1
+            s2Count[ord(s2[i]) - ord('a')]+=1
+        
+        matches = 0
+        for i in range(26):
+            matches+= (1 if s1Count[i]==s2Count[i] else 0)
         
         l=0
         for r in range(len(s1),len(s2)):
-            arr2[ord(s2[r])-ord('a')]+=1
-            arr2[ord(s2[l])-ord('a')]-=1
+            if matches == 26:
+                return True
+            index = ord(s2[r]) - ord('a')
+            s2Count[index]+=1
+            if s1Count[index] == s2Count[index]:
+                matches+=1
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches-=1
+            
+            index = ord(s2[l]) - ord('a')
+            s2Count[index]-=1
+            if s1Count[index] == s2Count[index]:
+                matches+=1
+            elif s1Count[index] - 1 == s2Count[index]: 
+                matches-=1
+
             l+=1
         
-            if arr1==arr2:
-                return True
-              
-      
-        return False
+        return matches == 26
             
-        
-
-        
