58d15bd93af7be57d9211b3b55987aa08686a033
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-03-25 21:04:37 -0400

    Time: 4570 ms (42.9%), Space: 17.8 MB (87.14%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,27 +1,29 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows = len(board)
-        cols = len(board[0])
-        visited = set()
+        ROWS = len(board)
+        COLS = len(board[0])
+        path = set()
+
         def backtrack(r,c,i):
             if i == len(word):
                 return True
-            if (r < 0 or c < 0 or
-                r>=rows or c>=cols or
-                word[i]!=board[r][c] or
-                (r,c) in visited):
+            if (r < 0 or c<0 or 
+                r>=ROWS or c>=COLS or
+                word[i]!=board[r][c] or (r,c) in path):
                 return False
-            visited.add((r,c))
-            result = (backtrack(r,c+1,i+1) or
-                      backtrack(r+1,c,i+1) or
-                      backtrack(r-1,c,i+1)or 
-                      backtrack(r,c-1,i+1))
-            visited.remove((r,c))
-            return result
+            
+            path.add((r,c))
+            res = (backtrack(r+1,c,i+1) or
+                  backtrack(r,c+1,i+1) or
+                    backtrack(r-1,c,i+1) or
+                    backtrack(r,c-1,i+1))
 
-        for r in range(rows):
-            for c in range(cols):
-                if backtrack(r,c,0): return True
+            path.remove((r,c))
+            return res
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if backtrack(r,c,0):
+                    return True
+
         return False
-
-        
