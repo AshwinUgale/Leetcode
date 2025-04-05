968036434142abe5f02ad3037bce26bf8b956dac
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-04-05 11:24:52 -0400

    Time: 23 ms (64.08%), Space: 23.4 MB (41.4%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -14,13 +14,13 @@ class Solution:
         # for i in range(len(nums)):
         #     arr[i]=lp[i]*rp[i]
         # return arr
-        res=[]
+        res=[1]*len(nums)
         c=1
         for i in range(len(nums)):
-            res.append(c)
+            res[i]=c
             c=c*nums[i]
         c=1
         for i in range(len(nums)-1,-1,-1):
-            res[i]=res[i]*c
+            res[i]*=c
             c=c*nums[i]
         return res
