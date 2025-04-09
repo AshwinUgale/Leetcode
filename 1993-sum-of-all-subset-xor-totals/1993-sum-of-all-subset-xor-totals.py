77c6219349b3c9b782ab935bd60454f2ecd2f58e
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-04-09 17:36:31 -0400

    Time: 13 ms (43.95%), Space: 17.9 MB (40.08%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
@@ -1,16 +1,10 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        total=0
-        self.sum=0
         def backtrack(total,i):
             if i == len(nums):
-                self.sum+=total
                 return total
-            total^=nums[i]
-            backtrack(total,i+1)
-            total^=nums[i]
-            backtrack(total,i+1)
-        backtrack(0,0)
-        return self.sum
+            return backtrack(total^nums[i],i+1)+backtrack(total,i+1)
+        
+        return backtrack(0,0)
 
 
