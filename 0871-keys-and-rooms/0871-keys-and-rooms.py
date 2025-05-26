9f0689beb4058c23465133a4420126ea1f927ba4
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-05-26 15:39:31 -0400

    Time: 75 ms (5.15%), Space: 18.8 MB (9.25%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.py
+++ 0871-keys-and-rooms/0871-keys-and-rooms.py
@@ -1,16 +1,12 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        adj=defaultdict(list)
-        for i in range(len(rooms)):
-            for j in rooms[i]:
-                adj[i].append(j)
         visited=set()
         def dfs(n):
             if n in visited:
                 return
             visited.add(n)
             print(visited)
-            for r in adj[n]:
+            for r in rooms[n]:
                 dfs(r)
             
         dfs(0)
