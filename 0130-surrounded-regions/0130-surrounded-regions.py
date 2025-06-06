cb5a4ce1d1f330ee5c82c9d595a696e6693caead
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-06-06 11:24:04 -0400

    Time: 3 ms (90.71%), Space: 21.5 MB (91.18%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,32 +1,29 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
-        ROWS = len(board)
-        COLS = len(board[0])
+        rows = len(board)
+        cols = len(board[0])
+        
 
-        def countS(r,c):
-            if (r < 0 or c < 0 or r == ROWS or c == COLS
-                or board[r][c]!="O") :
+        def dfs(r,c):
+            if (r<0 or c<0 or r==rows or c == cols or board[r][c]!="O"):
                 return 
-            board[r][c] = "T"
-            countS(r+1,c)
-            countS(r-1,c)
-            countS(r,c+1)
-            countS(r,c-1)
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (board[r][c] == "O" and 
-                    (r in [0,ROWS-1] or c in  [0,COLS-1])):
-                    countS(r,c)
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "O" :
-                    board[r][c] = "X"
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "T":
-                    board[r][c] = "O"
-
-            
+          
+            board[r][c]="T"
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+        for r in range(rows):
+            for c in range(cols):
+                if (r==0 or c==0 or r==rows-1 or c==cols-1):
+                    if board[r][c]=="O" :
+                        dfs(r,c)
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c]=="O":
+                    board[r][c]="X"
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c]=="T":
+                    board[r][c]="O"
+    
