8b96cb0021d8af9853b76aea6cb6a8c6ce10ca9e
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-06-20 14:32:16 -0400

    Time: 763 ms (55.08%), Space: 18.1 MB (68.86%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,21 +1,42 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coins.sort()
-        memo = {0:0}
 
-        def minCoin(amt):
-            if amt in memo:
-                return memo[amt]
+        #Top down memoization 
+        
+        # coins.sort()
+        # memo = {0:0}
+
+        # def minCoin(amt):
+        #     if amt in memo:
+        #         return memo[amt]
+        #     cur = float('inf')
+        #     for c in coins:
+        #         diff = amt - c
+        #         if diff < 0:
+        #             break
+        #         cur = min(cur,1+minCoin(diff))
+        #     memo[amt]= cur
+        #     return cur
+        # result = minCoin(amount)
+        # if result < float('inf'):
+        #     return result
+        # else:
+        #     return -1
+
+
+        #Bottom Up
+        coins.sort()
+        dp=[0]*(amount+1)
+        for i in range(1,amount+1):
             cur = float('inf')
             for c in coins:
-                diff = amt - c
+                diff = i - c
                 if diff < 0:
                     break
-                cur = min(cur,1+minCoin(diff))
-            memo[amt]= cur
-            return cur
-        result = minCoin(amount)
-        if result < float('inf'):
-            return result
+                cur = min(cur,dp[diff]+1)
+            dp[i]=cur
+        
+        if dp[amount]<float('inf'):
+            return dp[amount]
         else:
             return -1
