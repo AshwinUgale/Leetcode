0a1ebca2ed735a81c1f75236c88afddb84a212be
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-06-30 19:15:21 -0400

    Time: 79 ms (53.06%), Space: 18.3 MB (34.53%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,29 +1,23 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == "": return ""
-
-        countT, window = {}, {}
+        tcount={}
         for c in t:
-            countT[c] = 1 + countT.get(c,0)
-
-        have, need = 0 , len(countT)
-        res,resLen = [-1,-1], float("infinity")
+            tcount[c]=1+tcount.get(c,0)
+        scount={}
         l=0
+        need=len(tcount)
+        have=0
+        res=[0,float("inf")]
         for r in range(len(s)):
-            c=s[r]
-            window[c] = 1+ window.get(c,0)
-
-            if c in countT and window[c] == countT[c]:
+            scount[s[r]]=1+scount.get(s[r],0)
+            if s[r] in tcount.keys() and scount[s[r]]==tcount[s[r]]:
                 have+=1
-            
-            while have == need:
-                if (r-l+1) < resLen:
-                    res=[l,r]
-                    resLen = r-l+1
-
-                window[s[l]] -= 1
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
+            while need==have:
+                if (r-l+1)<res[1]-res[0]:
+                    res=[l,r+1]
+                scount[s[l]]=scount.get(s[l])-1
+                if s[l] in tcount.keys() and scount[s[l]]<tcount[s[l]]:
                     have-=1
                 l+=1
-        l,r=res
-        return s[l:r+1] if resLen!=float("infinity") else ""
+        return s[res[0]:res[1]] if res[1]!=float("inf") else ""
+
