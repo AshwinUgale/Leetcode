6e9d0e6a8fb7949f0afa198931fadad683f9cffb
AshwinUgale <74730738+AshwinUgale@users.noreply.github.com>
2025-06-30 19:15:49 -0400

    Time: 71 ms (65.44%), Space: 18.2 MB (77.31%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -10,13 +10,13 @@ class Solution:
         res=[0,float("inf")]
         for r in range(len(s)):
             scount[s[r]]=1+scount.get(s[r],0)
-            if s[r] in tcount.keys() and scount[s[r]]==tcount[s[r]]:
+            if s[r] in tcount and scount[s[r]]==tcount[s[r]]:
                 have+=1
             while need==have:
                 if (r-l+1)<res[1]-res[0]:
                     res=[l,r+1]
                 scount[s[l]]=scount.get(s[l])-1
-                if s[l] in tcount.keys() and scount[s[l]]<tcount[s[l]]:
+                if s[l] in tcount and scount[s[l]]<tcount[s[l]]:
                     have-=1
                 l+=1
         return s[res[0]:res[1]] if res[1]!=float("inf") else ""
