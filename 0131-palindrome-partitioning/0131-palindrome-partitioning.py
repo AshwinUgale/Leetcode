2df1ff104359fcea6eb5dd9b37aa7210154e5f2b
Ashwin Govind Ugale <74730738+AshwinUgale@users.noreply.github.com>
2025-07-16 20:14:13 -0400

    Time: 59 ms (25.09%), Space: 34.5 MB (44.19%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,24 +1,23 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        res = []
-        arr = []
-        def backtrack(i):
-            if i == len(s):
+        res=[]
+        arr=[]
+        
+        def ispalindrome(start,end):
+            return s[start:end+1]==s[start:end+1][::-1]
+
+        def backtrack(start):
+            if start==len(s):
                 res.append(arr.copy())
                 return
-            for j in range(i,len(s)):
-                if self.ispalin(s[i:j+1]):
-                    arr.append(s[i:j+1])
-                    backtrack(j+1)
+            for end in range(start,len(s)):
+                if ispalindrome(start,end):
+                    arr.append(s[start:end+1])
+                    backtrack(end+1)
                     arr.pop()
         backtrack(0)
         return res
-    def ispalin(self,s):
-        l=0
-        r=len(s)-1
-        while l<=r:
-            if s[l] != s[r]:
-                return False
-            l+=1
-            r-=1
-        return True
+            
+
+        
+                
