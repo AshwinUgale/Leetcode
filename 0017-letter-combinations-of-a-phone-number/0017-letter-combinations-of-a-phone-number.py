d6755d50f4af5efa9b96ed95fae42e915a32b99f
Ashwin Govind Ugale <74730738+AshwinUgale@users.noreply.github.com>
2025-07-16 20:30:14 -0400

    Time: 0 ms (100%), Space: 17.7 MB (68.92%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,22 +1,25 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        res = []
-        digitsToChar = { "2": "abc",
-                   "3": "def",
-                   "4": "ghi",
-                   "5": "jkl",
-                   "6": "mno",
-                   "7": "pqrs",
-                   "8": "tuv",
-                   "9": "wxyz"}
-        def backtrack(i,curStr):
-            if len(curStr) == len(digits):
-                res.append(curStr)
+        if not digits:
+            return []
+        numToLetter = {"2":"abc",
+                       "3":"def",
+                       "4":"ghi",
+                       "5":"jkl",
+                       "6":"mno",
+                       "7":"pqrs",
+                       "8":"tuv",
+                       "9":"wxyz"}
+
+        res=[]
+     
+        def backtrack(i,path):
+            if i==len(digits):
+                res.append("".join(path))
                 return
-            
-            for c in digitsToChar[digits[i]]:
-                backtrack(i+1,curStr+c)
-        if digits:
-            backtrack(0,"")
+            for c in numToLetter[digits[i]]:
+                path.append(c)
+                backtrack(i+1,path)
+                path.pop()
+        backtrack(0,[])
         return res
-            
