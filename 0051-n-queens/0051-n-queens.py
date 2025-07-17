f9de885f22b4c6cc7fc1ecfe5b0913d8ae676c25
Ashwin Govind Ugale <74730738+AshwinUgale@users.noreply.github.com>
2025-07-16 22:09:32 -0400

    Time: 11 ms (72.82%), Space: 18.5 MB (15.13%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,27 +1,28 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        cset=set()
-        pdia=set()
-        ndia=set()
-        res = []
-        board=[["."]*n for i in range(n)]
-
+        colSet=set()
+        pdiag=set()
+        ndiag=set()
+        board=[['.' for i in range(n)] for i in range(n)]
+        res=[]
         def backtrack(r):
-            if r == n:
-                copy = ["".join(row) for row in board]
-                res.append(copy)
+            if r==n:
+                res.append(["".join(row) for row in board])
                 return
             for c in range(n):
-                if c in cset or (r+c) in pdia or (r-c) in ndia:
+                if (c in colSet or (r-c) in ndiag or
+                    (r+c) in pdiag ):
                     continue
-                cset.add(c)
-                pdia.add(r+c)
-                ndia.add(r-c)
                 board[r][c]="Q"
+                colSet.add(c)
+                ndiag.add(r-c)
+                pdiag.add(r+c)
                 backtrack(r+1)
-                cset.remove(c)
-                pdia.remove(r+c)
-                ndia.remove(r-c)
                 board[r][c]="."
+                colSet.remove(c)
+                pdiag.remove(r+c)
+                ndiag.remove(r-c)
         backtrack(0)
+        
         return res
+            
