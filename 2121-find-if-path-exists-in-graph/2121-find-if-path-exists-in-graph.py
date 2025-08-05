a02ea18f8584d58c23e251c564158d01c78a8dd3
Ashwin Govind Ugale <74730738+AshwinUgale@users.noreply.github.com>
2025-08-05 11:46:36 -0400

    Time: 668 ms (26%), Space: 140 MB (20.46%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
@@ -1,21 +1,37 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        if n ==1:
-            return True
-        adj=defaultdict(list)
+        adjL=defaultdict(list)
         for n1,n2 in edges:
-            adj[n1].append(n2)
-            adj[n2].append(n1)
-        visited=set()
-        print(adj)
-        def dfs(node):
-            if node == destination:
+            adjL[n1].append(n2)
+            adjL[n2].append(n1)
+        visited = set()
+        def dfs(n):
+            if n == destination:
                 return True
-            visited.add(node)
-            for neighbor in adj[node]:
-                if neighbor not in visited:
-                    if dfs(neighbor):
+            visited.add(n)
+            for nei in adjL[n]:
+                if nei not in visited:
+                    if dfs(nei):
                         return True
             return False
         return dfs(source)
+        
+        
+        
+        #DFS
+        # q = deque([source])
+        # visited = set()
+        # def bfs():
+        #     while q:
+        #         val=q.popleft()
+        #         if val == destination:
+        #             return True
+        #         if val in visited:
+        #             continue
+        #         visited.add(val)
+        #         for nei in adjL[val]:
+        #             if nei not in visited:
+        #                 q.append(nei)
+        #     return False
+        # return bfs()
             
