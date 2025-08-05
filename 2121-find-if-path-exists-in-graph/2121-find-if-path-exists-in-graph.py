6136ff28fabf6fd0f3e62a273ddfae4d09e03df9
Ashwin Govind Ugale <74730738+AshwinUgale@users.noreply.github.com>
2025-08-05 11:47:07 -0400

    Time: 335 ms (78.4%), Space: 96.7 MB (68.96%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
@@ -4,34 +4,34 @@ class Solution:
         for n1,n2 in edges:
             adjL[n1].append(n2)
             adjL[n2].append(n1)
-        visited = set()
-        def dfs(n):
-            if n == destination:
-                return True
-            visited.add(n)
-            for nei in adjL[n]:
-                if nei not in visited:
-                    if dfs(nei):
-                        return True
-            return False
-        return dfs(source)
+        # visited = set()
+        # def dfs(n):
+        #     if n == destination:
+        #         return True
+        #     visited.add(n)
+        #     for nei in adjL[n]:
+        #         if nei not in visited:
+        #             if dfs(nei):
+        #                 return True
+        #     return False
+        # return dfs(source)
         
         
         
         #DFS
-        # q = deque([source])
-        # visited = set()
-        # def bfs():
-        #     while q:
-        #         val=q.popleft()
-        #         if val == destination:
-        #             return True
-        #         if val in visited:
-        #             continue
-        #         visited.add(val)
-        #         for nei in adjL[val]:
-        #             if nei not in visited:
-        #                 q.append(nei)
-        #     return False
-        # return bfs()
+        q = deque([source])
+        visited = set()
+        def bfs():
+            while q:
+                val=q.popleft()
+                if val == destination:
+                    return True
+                if val in visited:
+                    continue
+                visited.add(val)
+                for nei in adjL[val]:
+                    if nei not in visited:
+                        q.append(nei)
+            return False
+        return bfs()
             
